74de31e8b85473e392484407a5b910c703997663
siddarth kotthur <kotthursiddarth@gmail.com>
2023-06-29 15:01:43 -0500

    youtube tab switch works

---
+++ contentScript.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener((obj, sender, response) => {
+  const { type, value, videoId } = obj;
+
+  if (type === "NEW") {
+    const HTML_TEXT = document.body;
+  }
+});
+
+const addTranslateButton = (commentNode) => {};
